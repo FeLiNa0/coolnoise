c514ea4d5ea4cabe5e9b81b0e96fa7df72538990
Hugo Rivera <hugo.rivera2319@gmail.com>
2016-09-18 12:11:53 -0600

    Add command line option interface.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,35 @@
 module Main where
 
-import Data.Char (toLower)
+import System.IO (hFlush, stdout)
+
+import Data.Monoid
+import Options.Applicative
+
 import Cool.Types
 import Cool.Parse
+import Cool.Lex
+
+data Options = Options { fnames :: [FilePath]
+                       , inputs :: [String]
+                       } deriving Show
+
+manyFs = Options
+  <$> many (strOption (long "file" <> short 'f' <> metavar "FILENAME"
+            <> help "Use the contents of these .cl or .cool files."))
+  <*> many (strOption (long "input" <> short 'i' <> metavar "COOLSTRING"
+            <> help "Use this input string."))
+
+optParser = info (manyFs <**> helper)
+  (fullDesc <> header "coolnoise - COOL program lexer, parser and generator")
 
 main :: IO ()
 main = do
-  putStr "Enter filename > "
-  fname <- getLine
-  parseFileWith fname coolLexer >>= printLexResult 
+  options <- execParser optParser
+  mapM_ (\fname -> do
+           putStrLn (fname ++ ":")
+           result <- parseFileWith fname coolLexer
+           printLexResult result) (fnames options)
+  mapM_ (\input -> do
+           putStrLn ("<stdin> '" ++ input ++ "':")
+           let result = parseStringWith "<stdin>" input coolLexer
+           printLexResult result) (inputs options)
