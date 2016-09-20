67893120bcf3fa93fb8814e3f09d05b7677c7740
Hugo Rivera <hugo.rivera2319@gmail.com>
2016-09-19 18:05:21 -0600

    Added TypeID. Formatting.

--- src/Cool/Types.hs
+++ src/Cool/Types.hs
@@ -5,35 +5,38 @@ import Data.List.NonEmpty
 type ID = String
 type Type = String
 
-data ArithOp =
-  Add | Sub | Mul | Div
+data ArithOp
+  = Add | Sub | Mul | Div
   deriving (Show, Eq)
+
 data BoolOp
   = Lt | Le | Eq
   deriving (Show, Eq)
 
-data Expr = Integer Integer
-          | String String
-          | Bool Bool
-          | ID ID
-          | New ID
+data Expr
+  = Integer Integer
+  | String String
+  | Bool Bool
+  | ID ID
+  | TypeID Type
+  | New Type
 
-          | Assign ID  Expr
-          | IsVoid     Expr
-          | IntNegate  Expr
-          | BoolNegate Expr
-          | Paren      Expr
+  | Assign ID  Expr
+  | IsVoid     Expr
+  | IntNegate  Expr
+  | BoolNegate Expr
+  | Paren      Expr
 
-          | Op ArithOp Expr Expr
-          | BOp BoolOp Expr Expr
-          | While      Expr Expr
+  | Op ArithOp Expr Expr
+  | BOp BoolOp Expr Expr
+  | While      Expr Expr
 
-          | IfThenElse Expr Expr Expr
+  | IfThenElse Expr Expr Expr
 
-          | Call ID [Expr]
+  | Call ID [Expr]
 
-          | Sequence               (NonEmpty Expr)
-          | Static ID (Maybe Type) Expr [Expr]
+  | Sequence               (NonEmpty Expr)
+  | Static ID (Maybe Type) Expr [Expr]
 
-          | Case           Expr (NonEmpty (ID, Type, Expr))
-          | Let            Expr (NonEmpty (ID, Type, Expr))
+  | Case           Expr (NonEmpty (ID, Type, Expr))
+  | Let            Expr (NonEmpty (ID, Type, Expr))
